af4cebbc6f1422d4e016953d44f6428d1e5eb817
Владимир Половцев <ruck2424@mail.ru>
2016-11-02 20:13:58 +0300

    little bugfix

--- protected/Controllers/Bikes.php
+++ protected/Controllers/Bikes.php
@@ -13,6 +13,7 @@ use T4\Mvc\Controller;
  * @package App\Controllers
  * 
  * @TODO отображение картинок на /fdgsdfg/dfsgsdf
+ * @TODO инкапсулировать всё под итемы, невозможный говнокод вообще
  */
 class Bikes
     extends Controller
--- protected/routes.php
+++ protected/routes.php
@@ -6,8 +6,4 @@ return [
     '/bikes/<1>' => '//Bikes/Category(cat=<1>)',
     '/bikes/<2>' => '//Bikes/OneItem(url=<2>)',
     '/admin' => '//Admin/Default',
-    '/accessories/<3>' => '//Accessories/Category(cat=<3>)',
-    '/accessories/<4>' => '//Accessories/OneItem(url=<4>)',
-    '/clothing/<1>' => '//Clothing/Category(cat=<1>)',
-    '/clothing/<2>' => '//Clothing/OneItem(url=<2>)',
 ];
